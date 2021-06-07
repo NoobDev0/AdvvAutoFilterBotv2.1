887340660cc94b2c993d1db31c4441b7826ee424
TheProfessorTG <85480544+TheProfessorTG@users.noreply.github.com>
2021-06-07 14:55:55 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,14 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b><u>Hey {}!!</b></u>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+ <b>I Am Created to Serve <i>Ask A To Z Movies Group</i>☺</b>
+ You Can't Add Me To Any Group 😏
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+Join Our Official Channel
+@aska2zmovies
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +49,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Cyberrocker">The_Professor</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/TheProfessorTG/AdvAutoFilterBotv2">Click Me</a></i>
 """
